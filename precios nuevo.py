ab72941fe7b6ca41c8f7ce653a7c661947043aff
Michelleroca <80795260+Michelleroca@users.noreply.github.com>
2021-05-25 19:02:10 -0500

    Add files via upload

---
+++ precios nuevo.py
@@ -0,0 +1,7 @@
+# Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.
+
+print(50,75,46,22,80,65,8)
+mayor = 80
+menor = 8
+print("El menor es " + str(menor)) 
+print("El mayor es " + str(mayor)) 
